0f510119b80abc6c53488ebbed5a06a89e5be37a
Bohdan <yurchenko.b.l@gmail.com>
2021-05-20 22:35:24 +0300

    added semicolons

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -23,10 +23,10 @@ export const Timer = (props) => {
         setTime(props.time);
       }
 
-    }, step)
+    }, step);
 
     return () => {
-      clearInterval(ticker)
+      clearInterval(ticker);
     }
   }, [time, isAutoStart, step, props]);
 
